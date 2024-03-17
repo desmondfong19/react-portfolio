348d579dcaae68af29d2eb21f3586ae3fe49c981
Desmond Fong <desmondfong@hotmail.my>
2024-03-17 20:26:01 +1100

    fix minor naming

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -6,8 +6,8 @@ export const Hero = () => {
   return (
     <section className={styles.container}>
         <div className={styles.content}> 
-            {/* <h1 className={styles.title}>Hi, I'm Jian Fong</h1> */}
-            <p className={styles.description}>Hi, I'm Jian Wee Fong,a front-end developer with a year of experience using React and Javascript. Reach out if you'd like to know more!</p>
+            <h1 className={styles.title}>Hi, I'm Jian Fong</h1>
+            <p className={styles.description}>A front-end developer with a year of experience using React and Javascript. Reach out if you'd like to know more!</p>
             <a href='mailto:desmondfong@hotmail.my'className={styles.contactBtn}>Contact Me</a>
         </div>
         <img src={getImageUrl("hero/hero.jpg")} 
